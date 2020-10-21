04c948535359c1dc2b094162f6149c80cf126944
lijiankou <lijiankou@bigo.sg>
2020-10-21 22:02:34 +0800

    update

--- web/predict.py
+++ web/predict.py
@@ -12,21 +12,28 @@ import pandas as pd
 import log
 import train
 
-def Predict(model_path, img_width, img_height):
-    model = load_model(model_path)
-    img = load_img("test.png")
-    #img = smart_resize(img, size = (img_height, img_width))
+def Predict(model, img_width, img_height, img_path):
+    img = load_img(img_path)
+    logger.info(img.size)
     img = img.resize((img_height, img_width))
     img = train.img_to_array(img).reshape(img_height, img_width, channels)
     test = img / 255.0
     test = test.reshape((1,) + test.shape)
     res = model.predict(test)
-    logger.info(res)
+    return res
 
+def PredictBatch(model, img_width, img_height, img_dir):
+    for i, fn in enumerate(os.listdir(img_dir)):
+        img_path = os.path.join(img_dir, fn)
+        res = Predict(model, img_width, img_height, img_path)
+        logger.info("path:{}, score:{}".format(img_path, res))
+ 
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     logger = log.GetLogger(log.logging.INFO)
     img_width, img_height, channels = 350, 350, 3
     input_shape = (img_width, img_height, channels)
-    path = "beauty_model"
-    Predict(path, img_width, img_height)
+    model_path = "beauty_model"
+    img_dir = "data/test"
+    model = load_model(model_path)
+    PredictBatch(model, img_width, img_height, img_dir)
